6b129a73fa97bae8bdf6ee68e38309f7b3bb126b
tony007x <aekanan2004@gmail.com>
2024-09-06 13:25:29 +0700

    .

--- my-app/src/routes/home/+page.svelte
+++ my-app/src/routes/home/+page.svelte
@@ -231,7 +231,7 @@
 	<!-- Footer sticks at the bottom -->
 	<footer class="w-full bg-gray-100">
 		<div class="flex justify-between bg-black/5 p-4 text-xs">
-			<span>© 2024 | Made with ❤️ by Tony, Gal1leo</span>
+			<span>© 2024 | Made with ❤️ by <a href="https://github.com/tony007x">Tony219y</a> , Gal1leo</span>
 			<span>Computer Science, King Mongkut's Institute of Technology Ladkrabang</span>
 		</div>
 	</footer>
