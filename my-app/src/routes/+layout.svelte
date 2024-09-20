75bf76c9661b3acb71d4b0e1f5af7ed89e8dad43
Gal1leo2 <66050977@kmitl.ac.th>
2024-09-20 13:49:14 +0700

    add analytics

--- my-app/src/routes/+layout.svelte
+++ my-app/src/routes/+layout.svelte
@@ -1,7 +1,11 @@
 <script>
 	import '../app.css';
-	import { injectSpeedInsights } from '@vercel/speed-insights/sveltekit';
+	import { dev } from '$app/environment';
 
+	import { injectSpeedInsights } from '@vercel/speed-insights/sveltekit';
+	import { inject } from '@vercel/analytics'
+
+	inject({ mode: dev ? 'development' : 'production' });
 	injectSpeedInsights();
 </script>
 
